3a0e126cc283a5aa313f99771481150af53e0c1f
Micaela Besa <micaela.besasso@hotmail.com>
2026-02-08 17:54:39 +0100

    commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { Menu } from './Pages/menu/menu';
 import { Routes } from '@angular/router';
+import { Home } from './Pages/home/home';
+import { Login } from './Pages/login/login';
+import { Registro } from './Pages/registro/registro';
+import { Reservas } from './Pages/reservas/reservas';
+import { Pedidos } from './Pages/pedidos/pedidos';
+import { MenuDetail } from './Pages/menu-detail/menu-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'landing', component: Home },
+    { path: 'menus', component: Menu },
+    { path: 'menu-detail', component: MenuDetail },
+    { path: 'login', component: Login },
+    { path: 'registro', component: Registro },
+    { path: 'reservas', component: Reservas },
+    { path: 'pedidos', component: Pedidos },
+];
